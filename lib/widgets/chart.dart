1260d83e447fab0f6e0608f649902e32cfe19a61
fztkm <forthune.0517@gmail.com>
2021-08-14 23:24:39 +0900

    過去一週間の日ごとの合計を求めるメソッドを追加

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/material.dart';
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTxAmount {
+    return List.generate(7, (index) {
+      final DateTime weekDay = DateTime.now().subtract(Duration(days: index));
+
+      double totalAmount = 0;
+      for (int i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalAmount += recentTransactions[i].amount;
+        }
+      }
+
+      print('weekDay: $weekDay | totalAmount: $totalAmount');
+
+      return {
+        'date': index,
+        'amount': 99.99,
+      };
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
